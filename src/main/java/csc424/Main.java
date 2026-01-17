8076fa87ee8614668335ab9be43284e38da559e7
Ali Farahani <afarahan@nu.edu>
2026-01-17 14:48:36 -0800

    change to file name

--- src/main/java/csc424/Main.java
+++ src/main/java/csc424/Main.java
@@ -2,7 +2,7 @@ package csc424;
 
 public class Main {
     public static void main(String[] args) {
-        OrderCalculator calc = new OrderCalculator();
+        Order calc = new Order();
         double sub = calc.subtotal(19.99, 2);
         double tax = calc.tax(sub, 0.08);
         double total = calc.total(sub, tax);
