8dce2e82eb0aea5073590065c0ba4e01d2dd07e3
Ali Farahani <afarahan@nu.edu>
2026-01-17 14:56:50 -0800

    name change

--- src/main/java/csc424/Main.java
+++ src/main/java/csc424/Main.java
@@ -2,7 +2,7 @@ package csc424;
 
 public class Main {
     public static void main(String[] args) {
-        Orders calc = new Order();
+        Orders calc = new Orders();
         double sub = calc.subtotal(19.99, 2);
         double tax = calc.tax(sub, 0.08);
         double total = calc.total(sub, tax);
