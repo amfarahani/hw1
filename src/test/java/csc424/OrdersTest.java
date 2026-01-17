5193b48f3ff1f635442370322b0ac7e485c6f681
Ali Farahani <afarahan@nu.edu>
2026-01-17 14:50:52 -0800

    object name change

--- src/test/java/csc424/OrdersTest.java
+++ src/test/java/csc424/OrdersTest.java
@@ -7,19 +7,19 @@ public class OrdersTest {
 
     @Test
     public void testSubtotal() {
-        OrderCalculator calc = new OrderCalculator();
+        Orders calc = new Order();
         assertEquals(40.0, calc.subtotal(10.0, 4), 0.0001);
     }
 
     @Test
     public void testTax() {
-        OrderCalculator calc = new OrderCalculator();
+        Orders calc = new Orders();
         assertEquals(8.0, calc.tax(100.0, 0.08), 0.0001);
     }
 
     @Test
     public void testTotal() {
-        OrderCalculator calc = new OrderCalculator();
+        Orders calc = new Orders();
         assertEquals(108.0, calc.total(100.0, 8.0), 0.0001);
     }
 }
